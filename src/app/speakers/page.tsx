e0bff5879c11d3064fa0c289b082b397771813c8
Jeethula <jeethula10@gmail.com>
2024-11-26 15:03:49 +0530

    Fix HTML entity in speaker titles and clean up commented-out speaker list

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -1,157 +1,3 @@
-{/* <h1>Speakers</h1>
-<ul>
-    <li>
-        <h2>A. Velumani</h2>
-        <p><strong>Title:</strong> Founder</p>
-        <p><strong>Organization:</strong> Thyrocare</p>
-    </li>
-    <li>
-        <h2>Abhishek Singh, IAS</h2>
-        <p><strong>Title:</strong> Additional Secretary</p>
-        <p><strong>Organization:</strong> MEITY</p>
-    </li>
-    <li>
-        <h2>Ananth Narayanan</h2>
-        <p><strong>Title:</strong> Co-founder and CEO</p>
-        <p><strong>Organization:</strong> Mensa Brands</p>
-    </li>
-    <li>
-        <h2>Andre Agassi</h2>
-        <p><strong>Title:</strong> Legendary Tennis Champion and Philanthropist</p>
-        <p><strong>Organization:</strong> N/A</p>
-    </li>
-    <li>
-        <h2>Asha Jadeja</h2>
-        <p><strong>Title:</strong> President</p>
-        <p><strong>Organization:</strong> Motwani Jadeja Family Foundation</p>
-    </li>
-    <li>
-        <h2>Ashish Chauhan</h2>
-        <p><strong>Title:</strong> MD and CEO</p>
-        <p><strong>Organization:</strong> National Stock Exchange of India</p>
-    </li>
-    <li>
-        <h2>BV Naidu</h2>
-        <p><strong>Title:</strong> Chairman</p>
-        <p><strong>Organization:</strong> Karnataka Digital Economy Mission (KDEM)</p>
-    </li>
-    <li>
-        <h2>Dr. Ekroop Caur, IAS</h2>
-        <p><strong>Title:</strong> Secretary to Government</p>
-        <p><strong>Organization:</strong> Dept. of Electronics, Information Technology, Biotechnology and Science & Technology, Govt. of Karnataka</p>
-    </li>
-    <li>
-        <h2>Dr. Sanjay Tyagi</h2>
-        <p><strong>Title:</strong> Director</p>
-        <p><strong>Organization:</strong> STPI Bangalore</p>
-    </li>
-    <li>
-        <h2>Dr. Selvakumar, IAS</h2>
-        <p><strong>Title:</strong> Principal Secretary</p>
-        <p><strong>Organization:</strong> Commerce and Industry Department, Karnataka government</p>
-    </li>
-    <li>
-        <h2>Dr. Xinhong Lim</h2>
-        <p><strong>Title:</strong> MD</p>
-        <p><strong>Organization:</strong> Gelephu Mindfulness City</p>
-    </li>
-    <li>
-        <h2>Ganesh Natarajan</h2>
-        <p><strong>Title:</strong> Executive Chairman and Founder</p>
-        <p><strong>Organization:</strong> 5F World</p>
-    </li>
-    <li>
-        <h2>Ja-NaE Duane</h2>
-        <p><strong>Title:</strong> Author</p>
-        <p><strong>Organization:</strong> N/A</p>
-    </li>
-    <li>
-        <h2>Jeff Hoffman</h2>
-        <p><strong>Title:</strong> Chairman of the Board</p>
-        <p><strong>Organization:</strong> Global Entrepreneurship Network</p>
-    </li>
-    <li>
-        <h2>K. Rajaraman</h2>
-        <p><strong>Title:</strong> Chairman</p>
-        <p><strong>Organization:</strong> International Financial Services Centres Authority (IFSCA)</p>
-    </li>
-    <li>
-        <h2>Kevin Parikh</h2>
-        <p><strong>Title:</strong> Chairman and CEO</p>
-        <p><strong>Organization:</strong> Avasant</p>
-    </li>
-    <li>
-        <h2>Kris Gopalakrishnan</h2>
-        <p><strong>Title:</strong> Chairman Axilor Ventures, Co-founder Infosys, President ISF</p>
-        <p><strong>Organization:</strong> N/A</p>
-    </li>
-    <li>
-        <h2>Mike Kayamori</h2>
-        <p><strong>Title:</strong> CEO</p>
-        <p><strong>Organization:</strong> ORO Bank</p>
-    </li>
-    <li>
-        <h2>Mohandas Pai</h2>
-        <p><strong>Title:</strong> Chairman</p>
-        <p><strong>Organization:</strong> Manipal Global Education & Chairman at Aarin Capital</p>
-    </li>
-    <li>
-        <h2>Naiyya Saggi</h2>
-        <p><strong>Title:</strong> Group Co-founder</p>
-        <p><strong>Organization:</strong> The Good Glamm Group</p>
-    </li>
-    <li>
-        <h2>Peter Hughes</h2>
-        <p><strong>Title:</strong> Founder and CEO</p>
-        <p><strong>Organization:</strong> Apex Group</p>
-    </li>
-    <li>
-        <h2>Prashanth Prakash</h2>
-        <p><strong>Title:</strong> Founding Partner</p>
-        <p><strong>Organization:</strong> Accel Partners</p>
-    </li>
-    <li>
-        <h2>Priyank Kharge</h2>
-        <p><strong>Title:</strong> Hon'ble Minister for Information Technology & Biotechnology and Rural Development & Panchayat Raj Govt. of Karnataka</p>
-        <p><strong>Organization:</strong> N/A</p>
-    </li>
-    <li>
-        <h2>Radhika Gupta</h2>
-        <p><strong>Title:</strong> MD & CEO</p>
-        <p><strong>Organization:</strong> Edelweiss Mutual Fund</p>
-    </li>
-    <li>
-        <h2>Ravi Venkatesan</h2>
-        <p><strong>Title:</strong> Chairman</p>
-        <p><strong>Organization:</strong> Global Energy Alliance for People and Planet</p>
-    </li>
-    <li>
-        <h2>S Krishnan, IAS</h2>
-        <p><strong>Title:</strong> Secretary</p>
-        <p><strong>Organization:</strong> MeitY, Govt. of India</p>
-    </li>
-    <li>
-        <h2>Sanjeev Gupta</h2>
-        <p><strong>Title:</strong> CEO</p>
-        <p><strong>Organization:</strong> Karnataka Digital Economy Mission (KDEM)</p>
-    </li>
-    <li>
-        <h2>Sanjeev Sanyal</h2>
-        <p><strong>Title:</strong> Member of the Economic Advisory Council to the Prime Minister of India</p>
-        <p><strong>Organization:</strong> N/A</p>
-    </li>
-    <li>
-        <h2>Sudhir Sethi</h2>
-        <p><strong>Title:</strong> Founder and Chairman</p>
-        <p><strong>Organization:</strong> Chiratae Ventures</p>
-    </li>
-    <li>
-        <h2>Vani Kola</h2>
-        <p><strong>Title:</strong> Founder & Director</p>
-        <p><strong>Organization:</strong> Kalaari Capital</p>
-    </li>
-</ul> */}
-
 function page() {
     return (
         <div>
@@ -269,7 +115,7 @@ function page() {
     </li>
     <li>
         <h2>Priyank Kharge</h2>
-        <p><strong>Title:</strong> Hon'ble Minister for Information Technology & Biotechnology and Rural Development & Panchayat Raj Govt. of Karnataka</p>
+        <p><strong>Title:</strong> Honble Minister for Information Technology & Biotechnology and Rural Development & Panchayat Raj Govt. of Karnataka</p>
         <p><strong>Organization:</strong> N/A</p>
     </li>
     <li>
@@ -304,7 +150,7 @@ function page() {
     </li>
     <li>
         <h2>Vani Kola</h2>
-        <p><strong>Title:</strong> Founder & Director</p>
+        <p><strong>Title:</strong> Founder &amp; Director</p>
         <p><strong>Organization:</strong> Kalaari Capital</p>
     </li>
 </ul> 
